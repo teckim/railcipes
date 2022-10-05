c373ce59638dc97d99d39cf0790a0552d6fe9830
BHDHakim <hakim.bhd@gmail.com>
2022-10-05 14:32:00 +0100

    Fix rubocop linting issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,5 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
 
-  def index
-  end
+  def index; end
 end
