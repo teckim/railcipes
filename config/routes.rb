0801d47f2c0c1c0635d9217b219ba62ec91e16a8
BHDHakim <hakim.bhd@gmail.com>
2022-10-05 08:28:35 +0100

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "application#index"
 end
