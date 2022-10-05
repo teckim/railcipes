ebeac0f086762599f693b3dc12ab9c38d95bcaee
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-10-05 00:16:20 +0000

    Create models for the Recipe Application

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :validatable
+  has_many :recipes, foreign_key: :user_id, dependent: :destroy
+  has_many :foods, foreign_key: :user_id, dependent: :destroy
+
+  validates :name, presence: true, length: { in: 3..10 }
 end
